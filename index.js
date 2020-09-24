b937b66adc6e1248ccae9d5a9aeb889f1993836d
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2020-09-24 21:26:51 +0200

    Change english api message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const phones = require('./phones/phones.json')
 
 app.listen(8080, () => {
- console.log("Servidor inicializado en el puerto 8080");
+ console.log("Server initialized on port 8080");
 });
 
 app.use(function(req, res, next) {
